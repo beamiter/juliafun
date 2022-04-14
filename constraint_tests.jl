a1a196527603d3f2f93c30427a0e06538cf1b9c5
yinj3 <yinj3@xiaopeng.com>
2022-04-14 21:04:20 +0800

    sounds good

--- constraint_tests.jl
+++ constraint_tests.jl
@@ -22,7 +22,7 @@ z = KnotPoint(x, u, t, h)
 #################################################
 
 p = 5
-A = @SMatrix rand(p, n+m)
+A = @SMatrix rand(p, n + m)
 b = @SVector rand(p)
 lin = LinearConstraint(n, m, A, b, Inequality())
 @show RD.evaluate(lin, z)
@@ -30,29 +30,34 @@ lin = LinearConstraint(n, m, A, b, Inequality())
 
 #################################################
 
-xc = SA[1,1,1]
-yc = SA[1,2,3]
-r = SA[1,1,1]
+xc = SA[1, 1, 1]
+yc = SA[1, 2, 3]
+r = SA[1, 1, 1]
 cir = CircleConstraint(n, xc, yc, r)
 c = zeros(3)
-@show RD.evaluate(cir,z)
+@show RD.evaluate(cir, z)
 @show RD.output_dim(cir)
+∇c = TO.gen_jacobian(cir)
+RD.jacobian!(cir, ∇c, c, z)
+@show ∇c
+@show size(∇c)
 
 #################################################
 
 p = 5
-A = @SVector rand(p)
+A = @SMatrix rand(p, 2)
 b = @SVector rand(p)
-lx = 0.1
-ly = 0.1
-off = OffsetLinearConstraint(n, m, A, b, Inequality(), lx, ly)
+l = 1.0
+off = OffsetLinearConstraint(n, m, A, b, Inequality(), l)
 @show RD.evaluate(off, z)
 @show RD.output_dim(off)
 c = zeros(RD.output_dim(off))
 RD.evaluate!(off, c, z)
 @test c ≈ RD.evaluate(off, z)
-∇c = zeros(2 * p, n)
+∇c = zeros(p, n)
 RD.jacobian!(off, ∇c, c, z)
 @show ∇c
 
 #################################################
+
+loop_for_display()
