1b90dfecc5ccc3e3e5b84c50d4943387b4c4a311
yinj3 <yinj3@xiaopeng.com>
2022-04-19 11:41:27 +0800

    ellipse transform ok

--- bicycle_constraints.jl
+++ bicycle_constraints.jl
@@ -1,23 +1,24 @@
+#####################################################
 struct OffsetCircleConstraint{P,T} <: StageConstraint
-    n::Int
-    m::Int
-    x::SVector{P,T}
-    y::SVector{P,T}
-    radius::SVector{P,T}
-    l::Float64
-    inds::SVector{3,Int}
-    function OffsetCircleConstraint{P,T}(n::Int, m::Int, xc::AbstractVector, yc::AbstractVector,
-        radius::AbstractVector, l::Float64, inds=1:3) where {P,T}
-        @assert length(xc) == length(yc) == length(radius) == P "Length must be equal"
-        new{P,T}(n, m, xc, yc, radius, l, inds)
-    end
+  n::Int
+  m::Int
+  x::SVector{P,T}
+  y::SVector{P,T}
+  radius::SVector{P,T}
+  l::Float64
+  inds::SVector{3,Int}
+  function OffsetCircleConstraint{P,T}(n::Int, m::Int, xc::AbstractVector, yc::AbstractVector,
+    radius::AbstractVector, l::Float64, inds=1:3) where {P,T}
+    @assert length(xc) == length(yc) == length(radius) == P "Length must be equal"
+    new{P,T}(n, m, xc, yc, radius, l, inds)
+  end
 end
 
 function OffsetCircleConstraint(n::Int, m::Int, xc::AbstractVector, yc::AbstractVector, radius::AbstractVector,
-    l::Float64, inds=1:3)
-    T = promote_type(eltype(xc), eltype(yc), eltype(radius))
-    P = length(xc)
-    OffsetCircleConstraint{P,T}(n, m, xc, yc, radius, l, inds)
+  l::Float64, inds=1:3)
+  T = promote_type(eltype(xc), eltype(yc), eltype(radius))
+  P = length(xc)
+  OffsetCircleConstraint{P,T}(n, m, xc, yc, radius, l, inds)
 end
 
 @inline TO.sense(::OffsetCircleConstraint) = Inequality()
@@ -27,83 +28,83 @@ end
 @inline RD.functioninputs(::OffsetCircleConstraint) = RD.StateOnly()
 
 function RD.evaluate(con::OffsetCircleConstraint, X::RD.DataVector)
-    xc = con.x
-    yc = con.y
-    r = con.radius
-    xi, yi, θi = con.inds
-    x = X[xi]
-    y = X[yi]
-    θ = X[θi]
-    l = con.l
-    -(x + l * cos(θ) .- xc) .^ 2 - (y + l * sin(θ) .- yc) .^ 2 + r .^ 2
+  xc = con.x
+  yc = con.y
+  r = con.radius
+  xi, yi, θi = con.inds
+  x = X[xi]
+  y = X[yi]
+  θ = X[θi]
+  l = con.l
+  -(x + l * cos(θ) .- xc) .^ 2 - (y + l * sin(θ) .- yc) .^ 2 + r .^ 2
 end
 
 function RD.evaluate!(con::OffsetCircleConstraint{P}, c, X::RD.DataVector) where {P}
-    xc = con.x
-    yc = con.y
-    r = con.radius
-    xi, yi, θi = con.inds
-    x = X[xi]
-    y = X[yi]
-    θ = X[θi]
-    l = con.l
-    for i = 1:P
-        c[i] = -(x + l * cos(θ) - xc[i])^2 - (y + l * sin(θ) - yc[i])^2 + r[i]^2
-    end
-    return nothing
+  xc = con.x
+  yc = con.y
+  r = con.radius
+  xi, yi, θi = con.inds
+  x = X[xi]
+  y = X[yi]
+  θ = X[θi]
+  l = con.l
+  for i = 1:P
+    c[i] = -(x + l * cos(θ) - xc[i])^2 - (y + l * sin(θ) - yc[i])^2 + r[i]^2
+  end
+  return nothing
 end
 
 function RD.jacobian!(con::OffsetCircleConstraint{P}, ∇c, c, z::RD.AbstractKnotPoint) where {P}
-    X = RD.state(z)
-    xc = con.x
-    yc = con.y
-    xi, yi, θi = con.inds
-    x = X[xi]
-    y = X[yi]
-    θ = X[θi]
-    l = con.l
-    s, c = sincos(θ)
-    for i = 1:P
-        ∇c[i, xi] = -2 * (x + l * c - xc[i])
-        ∇c[i, yi] = -2 * (y + l * s - yc[i])
-        ∇c[i, θi] = l * dot((∇c[i, xi], ∇c[i, yi]), (-s, c))
-    end
-    return nothing
+  X = RD.state(z)
+  xc = con.x
+  yc = con.y
+  xi, yi, θi = con.inds
+  x = X[xi]
+  y = X[yi]
+  θ = X[θi]
+  l = con.l
+  s, c = sincos(θ)
+  for i = 1:P
+    ∇c[i, xi] = -2 * (x + l * c - xc[i])
+    ∇c[i, yi] = -2 * (y + l * s - yc[i])
+    ∇c[i, θi] = l * dot((∇c[i, xi], ∇c[i, yi]), (-s, c))
+  end
+  return nothing
 end
 
 #########################################################
 struct OffsetLinearConstraint{S,P,W,T} <: StageConstraint
-    n::Int
-    m::Int
-    A::SizedMatrix{P,W,T,2,Matrix{T}}
-    b::SVector{P,T}
-    sense::S
-    l::Float64
-    inds::SVector{3,Int}
-    function OffsetLinearConstraint(n::Int, m::Int, A::StaticMatrix{P,W,T},
-        b::StaticVector{P,T},
-        sense::ConstraintSense,
-        l::Float64, inds=1:3) where {P,W,T}
-        @assert size(A, 1) == length(b) "Length of A, b must be equal"
-        @assert W == 2 "Only support two dimension now"
-        inds = SVector{3}(inds)
-        new{typeof(sense),P,W,T}(n, m, A, b, sense, l, inds)
-    end
+  n::Int
+  m::Int
+  A::SizedMatrix{P,W,T,2,Matrix{T}}
+  b::SVector{P,T}
+  sense::S
+  l::Float64
+  inds::SVector{3,Int}
+  function OffsetLinearConstraint(n::Int, m::Int, A::StaticMatrix{P,W,T},
+    b::StaticVector{P,T},
+    sense::ConstraintSense,
+    l::Float64, inds=1:3) where {P,W,T}
+    @assert size(A, 1) == length(b) "Length of A, b must be equal"
+    @assert W == 2 "Only support two dimension now"
+    inds = SVector{3}(inds)
+    new{typeof(sense),P,W,T}(n, m, A, b, sense, l, inds)
+  end
 end
 
 function OffsetLinearConstraint(n::Int, m::Int, A::AbstractMatrix, b::AbstractVector,
-    sense::S, l::Float64, inds=1:3) where {S<:ConstraintSense}
-    @assert size(A, 1) == length(b) "Length of A, b must be equal"
-    @assert size(A, 2) == 2 "Currently only support 2 dimensions"
-    p, q = size(A)
-    T = promote_type(eltype(A), eltype(b))
-    A = SizedMatrix{p,q,T}(A)
-    b = SVector{p,T}(b)
-    OffsetLinearConstraint(n, m, A, b, sense, l, inds)
+  sense::S, l::Float64, inds=1:3) where {S<:ConstraintSense}
+  @assert size(A, 1) == length(b) "Length of A, b must be equal"
+  @assert size(A, 2) == 2 "Currently only support 2 dimensions"
+  p, q = size(A)
+  T = promote_type(eltype(A), eltype(b))
+  A = SizedMatrix{p,q,T}(A)
+  b = SVector{p,T}(b)
+  OffsetLinearConstraint(n, m, A, b, sense, l, inds)
 end
 
 Base.copy(con::OffsetLinearConstraint) where {S} =
-    OffsetLinearConstraint(con.n, con.m, copy(con.A), copy(con.b), S(), con.l, con.inds)
+  OffsetLinearConstraint(con.n, con.m, copy(con.A), copy(con.b), S(), con.l, con.inds)
 
 @inline TO.sense(con::OffsetLinearConstraint) = con.sense
 @inline RD.output_dim(::OffsetLinearConstraint{<:Any,P}) where {P} = P
@@ -112,44 +113,44 @@ Base.copy(con::OffsetLinearConstraint) where {S} =
 RD.functioninputs(::OffsetLinearConstraint) = RD.StateOnly()
 
 function RD.evaluate(con::OffsetLinearConstraint{S,P}, X::RD.DataVector) where {S,P}
-    A = con.A
-    b = con.b
-    l = con.l
-    x, y, θ = X[con.inds]
-    x0 = x + l * cos(θ)
-    y0 = y + l * sin(θ)
-    c = zeros(P)
-    for i in 1:P
-        c[i] = dot(A[i, 1:2], (x0, y0))
-    end
-    c .-= b
+  A = con.A
+  b = con.b
+  l = con.l
+  x, y, θ = X[con.inds]
+  x0 = x + l * cos(θ)
+  y0 = y + l * sin(θ)
+  c = zeros(P)
+  for i in 1:P
+    c[i] = dot(A[i, 1:2], (x0, y0))
+  end
+  c .-= b
 end
 
 function RD.evaluate!(con::OffsetLinearConstraint{<:Any,P}, c, X::RD.DataVector) where {P}
-    A = con.A
-    b = con.b
-    l = con.l
-    x, y, θ = X[con.inds]
-    x0 = x + l * cos(θ)
-    y0 = y + l * sin(θ)
-    for i in 1:P
-        c[i] = dot(A[i, 1:2], (x0, y0))
-    end
-    c .-= b
-    return nothing
+  A = con.A
+  b = con.b
+  l = con.l
+  x, y, θ = X[con.inds]
+  x0 = x + l * cos(θ)
+  y0 = y + l * sin(θ)
+  for i in 1:P
+    c[i] = dot(A[i, 1:2], (x0, y0))
+  end
+  c .-= b
+  return nothing
 end
 
 function RD.jacobian!(con::OffsetLinearConstraint{<:Any,P}, ∇c, c, z::RD.AbstractKnotPoint) where {P}
-    X = RD.state(z)
-    A = con.A
-    l = con.l
-    xi, yi, θi = con.inds
-    θ = X[θi]
-    s, c = sincos(θ)
-    for i = 1:P
-        ∇c[i, xi] = A[i, 1]
-        ∇c[i, yi] = A[i, 2]
-        ∇c[i, θi] = l * dot(A[i, 1:2], (-s, c))
-    end
-    return nothing
+  X = RD.state(z)
+  A = con.A
+  l = con.l
+  xi, yi, θi = con.inds
+  θ = X[θi]
+  s, c = sincos(θ)
+  for i = 1:P
+    ∇c[i, xi] = A[i, 1]
+    ∇c[i, yi] = A[i, 2]
+    ∇c[i, θi] = l * dot(A[i, 1:2], (-s, c))
+  end
+  return nothing
 end
--- bicycle_sim.jl
+++ bicycle_sim.jl
@@ -1,101 +1,115 @@
 
 function loop_for_gif()
-    x0 = SA_F64[0, 0, 0, 0, 4, 0]
-    plt = plot([0, 20, 20, 0, 0], 2.0 * [-1.2, -1.2, 1.2, 1.2, -1.2])
-    anim = @animate for i in UnitRange(1, 40)
-        @show x0
-        bicycle = BicycleCar(:parallel_park, x0=x0)
-        solver = ALTROSolver(bicycle...)
-        solve!(solver)
-        X = states(solver)
-        U = controls(solver)
-        @show size(X)
-        @show size(U)
-        x0 = X[2]
-        p = plot(plt, [x[1] for x in X], [x[2] for x in X])
-        display(p)
-        sleep(5)
-    end
-    gif(anim, "anim_fps15.gif", fps=1)
+  x0 = SA_F64[0, 0, 0, 0, 4, 0]
+  plt = plot([0, 20, 20, 0, 0], 2.0 * [-1.2, -1.2, 1.2, 1.2, -1.2])
+  anim = @animate for i in UnitRange(1, 40)
+    @show x0
+    bicycle = BicycleCar(:parallel_park, x0=x0)
+    solver = ALTROSolver(bicycle...)
+    solve!(solver)
+    X = states(solver)
+    U = controls(solver)
+    @show size(X)
+    @show size(U)
+    x0 = X[2]
+    p = plot(plt, [x[1] for x in X], [x[2] for x in X])
+    display(p)
+    sleep(5)
+  end
+  gif(anim, "anim_fps15.gif", fps=1)
 end
 
-function ellipse!(x::Float64, y::Float64, a::Float64, b::Float64, θ::Float64, plt::P) where {P}
-    theta = (0.0:0.1:2*pi+0.1)
-    s, c = sincos(θ)
-    xt = t -> cos(t)
-    yt = t -> sin(t)
-    fx = t -> c * a * xt(t) - s * b * yt(t) + x
-    fy = t -> s * a * xt(t) + c * b * yt(t) + y
-    plot!(plt, [fx(i) for i in theta], [fy(i) for i in theta])
-    # X, Y = [a * cos(i) for i in theta], [b * sin(i) for i in theta]
-    # plot!(plt, c .* X .- s .* Y .+ x, s .* X .+ c .* Y .+ y)
+# (h, k): new center point, a: semimajor axes length, b: semiminor axes length,
+# ψ: rotation angle
+function ellipse!(h::Float64, k::Float64, a::Float64, b::Float64, ψ::Float64, plt::P) where {P}
+  theta = (0.0:0.1:2*pi+0.1)
+  s, c = sincos(ψ)
+  xt = t -> cos(t)
+  yt = t -> sin(t)
+  fx = t -> c * a * xt(t) - s * b * yt(t) + h
+  fy = t -> s * a * xt(t) + c * b * yt(t) + k
+  f = (x, y) -> (x / a)^2 + (y / b)^2
+  @testset "ellipse" begin
+    for i in theta
+      x = fx(i)
+      y = fy(i)
+      # x′ = dot((c, s), (x, y)) + dot((c, s), (-h, -k))
+      # y′ = dot((-s, c), (x, y)) + dot((-s, c), (-h, -k))
+      x′ = c * x + s * y + (-c * h - s * k)
+      y′ = -s * x + c * y + (s * h - c * k)
+      @test f(x′, y′) ≈ 1.0
+    end
+  end
+  plot!(plt, [fx(i) for i in theta], [fy(i) for i in theta])
+  # X, Y = [a * cos(i) for i in theta], [b * sin(i) for i in theta]
+  # plot!(plt, c .* X .- s .* Y .+ x, s .* X .+ c .* Y .+ y)
 end
 
 function circle!(x::Float64, y::Float64, r::Float64, plt::P) where {P}
-    theta = (0.0:0.1:2*pi+0.1)
-    plot!(plt, [r * cos(i) + x for i in theta], [r * sin(i) + y for i in theta])
+  theta = (0.0:0.1:2*pi+0.1)
+  plot!(plt, [r * cos(i) + x for i in theta], [r * sin(i) + y for i in theta])
 end
 
 function plot_circle_con!(c::C, plt::P) where {C<:Union{CircleConstraint,OffsetCircleConstraint},P}
-    for i in 1:RD.output_dim(c)
-        x = c.x[i]
-        y = c.y[i]
-        r = c.radius[i]
-        circle!(x, y, r, plt)
-    end
+  for i in 1:RD.output_dim(c)
+    x = c.x[i]
+    y = c.y[i]
+    r = c.radius[i]
+    circle!(x, y, r, plt)
+  end
 end
 
 function loop_for_display()
-    gr()
-    x0 = SA_F64[0, 0, 0, 0, 4, 0]
-    xf = SA[13, 0.8, deg2rad(0), 0, 0.1, 0]
-    plt = plot([0, 20], [-2.4, -2.4])
-    plot!(plt, [0, 20], [-2.0, -2.0])
-    plot!(plt, [0, 20], [2.4, 2.4])
-    scatter!(plt, [xf[1]], [xf[2]], marker_size=2, shape=:star5)
-    his_x = []
-    his_y = []
-    his_x_f = []
-    his_y_f = []
-    for i in UnitRange(1, 40)
-        @show x0
-        bicycle = BicycleCar(:parallel_park, x0=x0, xf=xf)
-        solver = ALTROSolver(bicycle...)
-        solve!(solver)
-        X = states(solver)
-        U = controls(solver)
-        p = plot(plt, [x[1] for x in X], [x[2] for x in X])
-        cons = get_constraints(bicycle[1])
-        l = 1.0
-        for con in cons
-            @show typeof(con)
-            if con isa LinearConstraint
-            elseif con isa BoundConstraint
-            elseif con isa CircleConstraint
-                plot_circle_con!(con, p)
-            elseif con isa OffsetLinearConstraint
-                l = con.l
-            elseif con isa OffsetCircleConstraint
-                plot_circle_con!(con, p)
-            end
-        end
-        @show size(X)
-        @show size(U)
-        push!(his_x, x0[1])
-        push!(his_y, x0[2])
-        push!(his_x_f, x0[1] + l * cos(x0[3]))
-        push!(his_y_f, x0[2] + l * sin(x0[3]))
-
-        # r = 1.0
-        # circle!(his_x[end], his_y[end], r, p)
-        # circle!(his_x_f[end], his_y_f[end], r, p)
-        scatter!(p, his_x, his_y)
-        scatter!(p, his_x_f, his_y_f)
-        ellipse!(9.0, 1.0, 3.0, 1.0, deg2rad(30.0), p)
-        ellipse!(8.0, 1.0, 3.0, 1.0, deg2rad(10.0), p)
-        # savefig(p, "pics/$(i)_haha.png")
-        display(p)
-        readline()
-        x0 = X[2]
+  gr()
+  x0 = SA_F64[0, 0, 0, 0, 4, 0]
+  xf = SA[13, 0.8, deg2rad(0), 0, 0.1, 0]
+  plt = plot([0, 20], [-2.4, -2.4], aspect_ratio=:equal)
+  plot!(plt, [0, 20], [-2.0, -2.0])
+  plot!(plt, [0, 20], [2.4, 2.4])
+  scatter!(plt, [xf[1]], [xf[2]], marker_size=2, shape=:star5)
+  his_x = []
+  his_y = []
+  his_x_f = []
+  his_y_f = []
+  for i in UnitRange(1, 40)
+    @show x0
+    bicycle = BicycleCar(:parallel_park, x0=x0, xf=xf)
+    solver = ALTROSolver(bicycle...)
+    solve!(solver)
+    X = states(solver)
+    U = controls(solver)
+    p = plot(plt, [x[1] for x in X], [x[2] for x in X])
+    cons = get_constraints(bicycle[1])
+    l = 1.0
+    for con in cons
+      @show typeof(con)
+      if con isa LinearConstraint
+      elseif con isa BoundConstraint
+      elseif con isa CircleConstraint
+        plot_circle_con!(con, p)
+      elseif con isa OffsetLinearConstraint
+        l = con.l
+      elseif con isa OffsetCircleConstraint
+        plot_circle_con!(con, p)
+      end
     end
+    @show size(X)
+    @show size(U)
+    push!(his_x, x0[1])
+    push!(his_y, x0[2])
+    push!(his_x_f, x0[1] + l * cos(x0[3]))
+    push!(his_y_f, x0[2] + l * sin(x0[3]))
+
+    # r = 1.0
+    # circle!(his_x[end], his_y[end], r, p)
+    # circle!(his_x_f[end], his_y_f[end], r, p)
+    scatter!(p, his_x, his_y)
+    scatter!(p, his_x_f, his_y_f)
+    ellipse!(9.0, 1.0, 3.0, 1.0, deg2rad(30.0), p)
+    ellipse!(8.0, 1.0, 3.0, 1.0, deg2rad(10.0), p)
+    # savefig(p, "pics/$(i)_haha.png")
+    display(p)
+    readline()
+    x0 = X[2]
+  end
 end
