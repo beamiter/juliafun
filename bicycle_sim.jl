e5b7a743dbfb090f7f8290e80679ba8bd47f0943
yinj3 <yinj3@xiaopeng.com>
2022-04-24 18:28:07 +0800

    polish nearest line segment projection

--- bicycle_sim.jl
+++ bicycle_sim.jl
@@ -110,7 +110,7 @@ function plot_ellipse_con!(
     b = c.b[i]
     ψ = c.ψ[i]
     if pred
-      ellipse!(h, k, a, b, ψ, plt, alpha = 0.3, color = :azure3)
+      # ellipse!(h, k, a, b, ψ, plt, alpha = 0.3, color = :azure3)
       ellipse!(h, k, a - 1.0, b - 1.0, ψ, plt, alpha = 0.3, color = :azure3)
     else
       ellipse!(h, k, a, b, ψ, plt, linewidth = 1.5)
@@ -133,6 +133,48 @@ function plot_circle_con!(c::C, plt::P) where {C<:Union{CircleConstraint,OffsetC
   end
 end
 
+function get_line_segments_points(
+  line_segments_x::Vector{T},
+  line_segments_y::Vector{T},
+  X,
+) where {T}
+  @assert length(line_segments_y) == length(line_segments_x)
+  seg_inds = map(X) do x
+    local dis = []
+    local x0, y0 = x[1:2]
+    for i = 1:(length(line_segments_x)-1)
+      x1 = line_segments_x[i]
+      y1 = line_segments_y[i]
+      push!(dis, (x1 - x0)^2 + (y1 - y0)^2)
+    end
+    @assert length(dis) == length(line_segments_x) - 1
+    argmin(dis)
+  end
+  line_segments_points = []
+  for (i, x) in enumerate(X)
+    x0, y0 = x[1:2]
+    id = seg_inds[i]
+    if id != 1 && id != length(X)
+      x_prev, y_prev = line_segments_x[id-1], line_segments_y[id-1]
+      x_cur, y_cur = line_segments_x[id], line_segments_y[id]
+      x_next, y_next = line_segments_x[id+1], line_segments_y[id+1]
+      x0_vec = (x0 - x_cur, y0 - y_cur)
+      prev_vec = (x_prev - x_cur, y_prev - y_cur)
+      next_vec = (x_next - x_cur, y_next - y_cur)
+      cosine1 = dot(x0_vec, prev_vec) / (prev_vec[1]^ 2 + prev_vec[2]^2)
+      cosine2 = dot(x0_vec, next_vec) / (next_vec[1]^ 2 + next_vec[2]^2)
+      if cosine1 > cosine2
+        id = id - 1
+      end
+    end
+    x_1, y_1 = line_segments_x[id], line_segments_y[id]
+    x_2, y_2 = line_segments_x[id+1], line_segments_y[id+1]
+    len = hypot(x_1 - x_2, y_1 - y_2)
+    push!(line_segments_points, [x_1, y_1, x_2, y_2, len])
+  end
+  line_segments_points
+end
+
 function loop_for_display()
   gr()
   x0 = SA_F64[0, 0, 0, 0, 4, 0]
@@ -146,7 +188,6 @@ function loop_for_display()
   line_segments_x = Vector{Float64}([0.0, 5.0, 10.0, 15.0])
   line_segments_y = Vector{Float64}([1.2, 1.2, 0.0, 0.0])
   @assert length(line_segments_x) == length(line_segments_y) "x, y length should be equal"
-  line_segments = collect(zip(line_segments_x, line_segments_y))
   plot!(plt, line_segments_x, line_segments_y)
   scatter!(plt, [xf[1]], [xf[2]], marker_size = 2, shape = :star5)
   his_x = []
@@ -160,7 +201,6 @@ function loop_for_display()
 
     X0 = SA_F64[]
     U0 = SA_F64[0, 0]
-    line_segment_points = []
     if warm_up
       # 1. use ilqr to warm up
       bicycle = BicycleCar(x0, xf, N, tf, i = i, constrained = false)
@@ -172,25 +212,14 @@ function loop_for_display()
         [x[1] for x in X],
         [x[2] for x in X],
         linewidth = 1.5,
-        linestyle = :dashdot,
+        linestyle = :dot,
         color = :blue,
       )
-      line_segment_points = map(X) do x
-        local dis = []
-        local x0, y0 = x[1:2]
-        for (x1, y1) in line_segments[1:end-1]
-          push!(dis, hypot(x1 - x0, y1 - y0))
-        end
-        local id = argmin(dis)
-        x1, y1 = line_segments[id]
-        x2, y2 = line_segments[id+1]
-        len = hypot(x2 - x1, y2 - y1)
-        [x1, y1, x2, y2, len]
-      end
       # 2. warm up
       X0 = states(solver)
       U0 = controls(solver)
     end
+    line_segment_points = get_line_segments_points(line_segments_x, line_segments_y, X0)
     bicycle = BicycleCar(
       x0,
       xf,
@@ -206,7 +235,14 @@ function loop_for_display()
     solver = ALTROSolver(bicycle...)
     solve!(solver)
     X = states(solver)
-    plot!(p, [x[1] for x in X], [x[2] for x in X], linewidth = 1.5, color = :green)
+    plot!(
+      p,
+      [x[1] for x in X],
+      [x[2] for x in X],
+      linestyle = :dot,
+      linewidth = 1.5,
+      color = :green,
+    )
 
     l = 2.0
     con_list = get_constraints(bicycle[1])
@@ -249,7 +285,7 @@ function loop_for_display()
     circle!(his_x[end], his_y[end], r, p)
     circle!(his_x_f[end], his_y_f[end], r, p)
     scatter!(p, his_x, his_y, markersize = 2)
-    scatter!(p, his_x_f, his_y_f, markersize = 2)
+    # scatter!(p, his_x_f, his_y_f, markersize = 2)
     # savefig(p, "pics/$(i)_haha.png")
     display(p)
     readline()
